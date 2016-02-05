d76cdfc2b6fb983d9ee32fb3bb6dca2e24df7b1b
Jawad Nawaz <jawadnawaz@live.com>
2016-02-05 15:36:52 +0100

    Member delet confirmation using Modal Done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,15 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Create New User by <a href="{{ url('member') }}" class="btn btn-link" onclick="unbindExit();">Create Now</a></div>
+                <div class="panel-heading">Create New User by <a href="{{ url('member') }}" class="btn btn-link">Create Now</a></div>
                 <div class="panel-body">
                     @if (! $totalRecords == 0)
+                    @include('forms.modalform')
                     @include('forms.pagination')
                     <table class="table table-hover">
                       <thead>
                         <tr>
+                          <th>#</th>
                           <th>User Name</th>
                           <th>User Email</th>
                           <th>Phone</th>
@@ -20,9 +22,11 @@
                           <th>Delete User</th>
                         </tr>
                       </thead>
+                      <?php $i = 1; ?>
                       @foreach ($members as $key => $value)
                        <tbody>
                         <tr>
+                          <td>{{ $i }}</td>
                           <td>{{ $value->name }}</td>
                           <td>{{ $value->email }}</td>
                           <td>{{ $value->phone }}</td>
@@ -30,14 +34,16 @@
                           <td>
                             <input type="radio" name="optradio" value="{{ $value->id }}">
                           </td>
-                          <td><a href="deletemember?id={{ $value->id }}" class="delete btn btn-link">Delete</a></td>
+                          <!-- <td><a href="deletemember?id={{ $value->id }}" class="delete btn btn-link">Delete</a></td> -->
+                          <td><button type="button" value="{{ $value->id }}" class="delete btn btn-danger" data-toggle="modal" data-target="#myModal">Delete</button></td>
                         </tr>
                        </tbody>
+                       <?php $i++; ?>
                       @endforeach
                     </table>
                     <p>Select a User From List and <a href="" id="update" class="btn btn-link">Click Here to Update</a></p>
                     @else
-                        <p>No Members Data You can <br/><a href="{{ url('member') }}" class="btn btn-link" onclick="unbindExit();">Create Now</a></p>
+                        <p>No Members Data You can <br/><a href="{{ url('member') }}" class="btn btn-link">Create Now</a></p>
                     @endif
                 </div>
             </div>
@@ -47,22 +53,11 @@
 @section('javascript')
 <script type="text/javascript">
 $('.delete').click(function () {
-var confirmOnPageExit = function (e)
-{
-    e = e || window.event;
-    var message = 'Are you Sure you Want to Delete This User . . .?';
-    if (e)
-    {
-      e.returnValue = message;
-    }
-    return message;
-};
-window.onbeforeunload = confirmOnPageExit;
-
+  var memberId = $(this).attr("value");
+  var link = document.getElementById("deleteLink");
+  var url = 'deletemember?id=' + memberId;
+  link.setAttribute('href', url);
 });
-function unbindExit() {
-    window.onbeforeunload = null;
-}
 
 $('#update').click(function () {
   if ( $("input[name='optradio']:checked").val() != undefined )
@@ -70,7 +65,6 @@ $('#update').click(function () {
     var link = document.getElementById("update");
     // var url = window.location.hostname + '/updatemember?id=' + $("input[name='optradio']:checked").val();
     var url = 'updatemember?id=' + $("input[name='optradio']:checked").val();
-    unbindExit();
     link.setAttribute('href', url);
     return true;
   }
@@ -80,9 +74,6 @@ $('#update').click(function () {
     return false;
   }
 });
-
-
-// var memberId = $(this).attr("value");
 </script>
 
 @stop
